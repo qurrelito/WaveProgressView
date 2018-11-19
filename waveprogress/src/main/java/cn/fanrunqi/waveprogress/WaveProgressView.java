23136f9658c5d7f7b6b010c13c0c04091b5e6987
qurrelito <40795250+qurrelito@users.noreply.github.com>
2018-11-19 15:06:31 +0100

    Update Waveprogress
    Prevent app from leaks
    Refactor of code.

--- waveprogress/src/main/java/cn/fanrunqi/waveprogress/WaveProgressView.java
+++ waveprogress/src/main/java/cn/fanrunqi/waveprogress/WaveProgressView.java
@@ -14,53 +14,59 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
 import android.view.View;
+import java.lang.ref.WeakReference;
 
 /**
+ * A custom view that creates a wave animation on a image
+ *
  * Created by fanrunqi on 2016/7/6.
  */
 public class WaveProgressView extends View {
-    private int mWidth;
-    private int mHeight;
+
+    private static Handler mHandler;
+
+    private static final int INVALIDATE = 0X777;
+
+    private static final int REFRESHGAP = 10;
 
     private Bitmap mBackgroundBitmap;
 
-    private Path mPath;
-    private Paint mPathPaint;
-
-    private float mWaveHeight = 20f;
-    private float mWaveHalfWidth = 100f;
-    private String mWaveColor = "#5be4ef";
-    private int mWaveSpeed = 30;
-
-    private Paint mTextPaint;
-    private String mCurrentText = "";
-    private String mTextColor = "#FFFFFF";
-    private int mTextSize = 41;
-
-    private int mMaxProgress = 100;
-    private int mCurrentProgress = 0;
     private float mCurY;
 
+    private int mCurrentProgress = 0;
+
+    private String mCurrentText = "";
+
     private float mDistance = 0;
-    private int mRefreshGap = 10;
 
-    private boolean mAllowProgressInBothDirections = false;
+    private int mHeight;
 
-    private static final int INVALIDATE = 0X777;
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what) {
-                case INVALIDATE:
-                    invalidate();
-                    sendEmptyMessageDelayed(INVALIDATE, mRefreshGap);
-                    break;
-            }
-        }
-    };
+    private Path mPath;
 
+    private Paint mPathPaint;
 
+    private String mTextColor = "#FFFFFF";
+
+    private Paint mTextPaint;
+
+    private int mTextSize = 41;
+
+    private String mWaveColor = "#5be4ef";
+
+    private float mWaveHalfWidth = 100f;
+
+    private float mWaveHight = 20f;
+
+    private int mWaveSpeed = 30;
+
+    private int mMaxProgress = 100;
+
+    private int mWidth;
+
+    /**
+     * Creates instance of WaveProgressView
+     * @param context context
+     */
     public WaveProgressView(Context context) {
         this(context, null, 0);
     }
@@ -71,77 +77,93 @@ public class WaveProgressView extends View {
 
     public WaveProgressView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-
-        Init();
+        init();
     }
 
+    /**
+     * Sets current progress
+     *
+     * @param currentProgress Progress value
+     * @param currentText     Text on the image.
+     */
     public void setCurrent(int currentProgress, String currentText) {
         this.mCurrentProgress = currentProgress;
         this.mCurrentText = currentText;
     }
 
+    /**
+     * Sets max progress
+     *
+     * @param maxProgress The max progress value, e.g 100
+     */
     public void setMaxProgress(int maxProgress) {
         this.mMaxProgress = maxProgress;
     }
 
-
-    public void setText(String mTextColor, int mTextSize) {
+    /**
+     * Sets text color and text size
+     *
+     * @param mTextColor The text color
+     * @param textSize  The text size
+     */
+    public void setText(String mTextColor, int textSize) {
         this.mTextColor = mTextColor;
-        this.mTextSize = mTextSize;
+        this.mTextSize = textSize;
     }
 
-    public void setWave(float mWaveHight, float mWaveWidth) {
-        this.mWaveHeight = mWaveHight;
-        this.mWaveHalfWidth = mWaveWidth / 2;
+    /**
+     * Sets the wave height and wave mWidth
+     *
+     * @param waveHeight The wave height
+     * @param waveWidth The wave mWidth
+     */
+    public void setWave(float waveHeight, float waveWidth) {
+        this.mWaveHight = waveHeight;
+        this.mWaveHalfWidth = waveWidth / 2;
     }
 
-
-    public void setWaveColor(String mWaveColor) {
-        this.mWaveColor = mWaveColor;
+    /**
+     * Sets the wave color
+     *
+     * @param waveColor The color
+     */
+    public void setWaveColor(String waveColor) {
+        this.mWaveColor = waveColor;
     }
 
+    /**
+     * Sets the wave speed
+     *
+     * @param mWaveSpeed the speed.
+     */
     public void setWaveSpeed(int mWaveSpeed) {
         this.mWaveSpeed = mWaveSpeed;
     }
 
-    public void allowProgressInBothDirections(boolean allow) {
-        this.mAllowProgressInBothDirections = allow;
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mHandler.sendEmptyMessage(INVALIDATE);
     }
 
-    private void Init() {
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mHandler.removeCallbacksAndMessages(mHandler);
+    }
 
-        if (null == getBackground()) {
-            throw new IllegalArgumentException(String.format("background is null."));
-        } else {
-            mBackgroundBitmap = getBitmapFromDrawable(getBackground());
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mBackgroundBitmap != null) {
+            canvas.drawBitmap(createImage(), 0, 0, null);
         }
-
-        mPath = new Path();
-        mPathPaint = new Paint();
-        mPathPaint.setAntiAlias(true);
-        mPathPaint.setStyle(Paint.Style.FILL);
-
-        mTextPaint = new Paint();
-        mTextPaint.setAntiAlias(true);
-        mTextPaint.setTextAlign(Paint.Align.CENTER);
-
-        handler.sendEmptyMessageDelayed(INVALIDATE, 100);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
         mWidth = MeasureSpec.getSize(widthMeasureSpec);
         mCurY = mHeight = MeasureSpec.getSize(heightMeasureSpec);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-
-        if (mBackgroundBitmap != null) {
-            canvas.drawBitmap(createImage(), 0, 0, null);
-        }
     }
 
     private Bitmap createImage() {
@@ -152,12 +174,9 @@ public class WaveProgressView extends View {
         Paint paint = new Paint();
         paint.setAntiAlias(true);
         Bitmap finalBmp = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
-
         Canvas canvas = new Canvas(finalBmp);
-
         float CurMidY = mHeight * (mMaxProgress - mCurrentProgress) / mMaxProgress;
-
-        if (mAllowProgressInBothDirections || mCurY > CurMidY) {
+        if (mCurY > CurMidY) {
             mCurY = mCurY - (mCurY - CurMidY) / 10;
         }
         mPath.reset();
@@ -165,10 +184,11 @@ public class WaveProgressView extends View {
 
         int waveNum = mWidth / ((int) mWaveHalfWidth * 4) + 1;
         int multiplier = 0;
-
-        for (int i = 0; i < waveNum; i++) {
-            mPath.quadTo(mWaveHalfWidth * (multiplier + 1) - mDistance, mCurY - mWaveHeight, mWaveHalfWidth * (multiplier + 2) - mDistance, mCurY);
-            mPath.quadTo(mWaveHalfWidth * (multiplier + 3) - mDistance, mCurY + mWaveHeight, mWaveHalfWidth * (multiplier + 4) - mDistance, mCurY);
+        for (int i = 0; i < waveNum * 3; i++) {
+            mPath.quadTo(mWaveHalfWidth * (multiplier + 1) - mDistance, mCurY - mWaveHight,
+                    mWaveHalfWidth * (multiplier + 2) - mDistance, mCurY);
+            mPath.quadTo(mWaveHalfWidth * (multiplier + 3) - mDistance, mCurY + mWaveHight,
+                    mWaveHalfWidth * (multiplier + 4) - mDistance, mCurY);
             multiplier += 4;
         }
         mDistance += mWaveHalfWidth / mWaveSpeed;
@@ -178,37 +198,75 @@ public class WaveProgressView extends View {
         mPath.lineTo(0, mHeight);
         mPath.close();
         canvas.drawPath(mPath, mPathPaint);
-
         int min = Math.min(mWidth, mHeight);
         mBackgroundBitmap = Bitmap.createScaledBitmap(mBackgroundBitmap, min, min, false);
-
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_ATOP));
-
         canvas.drawBitmap(mBackgroundBitmap, 0, 0, paint);
-
         canvas.drawText(mCurrentText, mWidth / 2, mHeight / 2, mTextPaint);
         return finalBmp;
     }
 
     private Bitmap getBitmapFromDrawable(Drawable drawable) {
-
         if (drawable == null) {
             return null;
         }
-
         if (drawable instanceof BitmapDrawable) {
             return ((BitmapDrawable) drawable).getBitmap();
         }
-
         try {
             Bitmap bitmap;
-            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            bitmap = Bitmap
+                    .createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(),
+                            Bitmap.Config.ARGB_8888);
             Canvas canvas = new Canvas(bitmap);
             drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
             drawable.draw(canvas);
             return bitmap;
         } catch (OutOfMemoryError e) {
             return null;
+        }
+    }
+
+    private void init() {
+        if (null == getBackground()) {
+            throw new IllegalArgumentException(String.format("background is null."));
+        } else {
+            mBackgroundBitmap = getBitmapFromDrawable(getBackground());
+        }
+        mPath = new Path();
+        mPathPaint = new Paint();
+        mHandler = new MyHandler(this);
+        mPathPaint.setAntiAlias(true);
+        mPathPaint.setStyle(Paint.Style.FILL);
+        mTextPaint = new Paint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+    }
+
+    private static class MyHandler extends Handler {
+
+        private final WeakReference<WaveProgressView> mView;
+
+        /**
+         * Creates instance of handler.
+         * @param view
+         */
+        public MyHandler(WaveProgressView view) {
+            mView = new WeakReference<>(view);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            WaveProgressView waveProgressView = mView.get();
+            if (waveProgressView != null) {
+                super.handleMessage(msg);
+                switch (msg.what) {
+                    case INVALIDATE:
+                        waveProgressView.invalidate();
+                        sendEmptyMessageDelayed(INVALIDATE, REFRESHGAP);
+                        break;
+                }
+            }
         }
     }
 }
